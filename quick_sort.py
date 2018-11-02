2095ceb7f27647dcb58c53b3f28ad1938338b7ae
pavel-kostyanov <kspavel777@gmail.com>
2018-11-02 18:10:52 +0300

    Codecademy quick-sort version

--- quick_sort.py
+++ quick_sort.py
@@ -1,5 +1,36 @@
+from random import randrange, shuffle
 unsort = [541, 22, 34, 5674, 24, 134, 84, 135, 5, 125, 456, 16, 7, 987, 1187, 47, 18, 69]
+"""
+******* CODECADEMY VERSION OF QUICK-SORT **************
+def quick_sort(arr, start, end):
+    if start >= end:
+        return
+      # select random element to be pivot
+    pivot_idx = randrange(start, end + 1)
+    pivot_element = arr[pivot_idx]
 
+     # swap random element with last element in sub-listay
+    arr[end], arr[pivot_idx] = arr[pivot_idx], arr[end]
+
+    less_than_pointer = start
+
+    for i in range(start, end):
+    # we found an element out of place
+        if arr[i] < pivot_element:
+    # swap element to the right-most portion of lesser elements
+            arr[i], arr[less_than_pointer] = arr[less_than_pointer], arr[i]
+            less_than_pointer += 1
+    # move pivot element to the right-most portion of lesser elements
+    arr[end], arr[less_than_pointer] =  arr[less_than_pointer], arr[end]
+    # Call quicksort on the "left" and "right" sub-lists
+    quick_sort(arr, start, less_than_pointer -1)
+    quick_sort(arr, less_than_pointer + 1, end)
+
+quick_sort(unsort, 0, len(unsort) - 1)
+print(unsort)
+"""
+"""
+******* MY OWN VERSION OF QUICK-SORT ***************
 def quick_sort(arr):
     if len(arr) > 1:
         x = arr[0]
@@ -10,9 +41,8 @@ def quick_sort(arr):
         return quick_sort(left) + mid + quick_sort(right)
     else:
         return arr
+"""
 
-
-quick_sort(unsorted_list)
 
 
 
